7039cd922c461ab6518c79d8fa9990e8450a7cc8
DatGuy <mcfun001@126.com>
2022-03-14 21:25:57 +0800

    optimized the style.

--- src/views/layout/FooterView.vue
+++ src/views/layout/FooterView.vue
@@ -3,6 +3,6 @@
     class="navbar bottom-NavBar"
     id="code-font"
   >
-    <p class="m-auto">Copyright © 2022 PRMLK. All rights reserved. </p>
+    <p class="m-auto allow-copy">Copyright © 2022 PRMLK. All rights reserved. </p>
   </footer>
 </template>
