e5ded0a2b2818084617695f483580be2b7ea0ed7
Lin Xin <widayn@prmlk.com>
2022-03-20 22:59:51 +0800

    bug动画

--- src/component/SideBar.vue
+++ src/component/SideBar.vue
@@ -1,10 +1,11 @@
 <template>
   <ul
-    class="nav flex-column nav-pills"
+    class="nav flex-column nav-pills child_right_slider transition_dom"
     id="v-pills-tab"
     role="tablist"
     aria-orientation="vertical"
     style="flex-direction: column;"
+    ref="box"
   >
     <li style="list-style: none;">
       <a
@@ -67,6 +68,8 @@ export default ({
     return {
       nextLevel: this.level + 1,
       visible: false,
+      is_show: true,
+      height: '',
     };
   },
   components: {
@@ -92,16 +95,43 @@ export default ({
     nowSelectID: (state) => state.treeMenusModule.sideBarNowSelectID,
   }),
   mounted() {
+    this.fun_get_list();
   },
   methods: {
     ...mapActions('treeMenusModule', { update: 'update', updateID: 'updateID' }),
     click(father, value) {
+      if (this.is_show) {
+        this.$refs.box.style.height = '0';
+      } else {
+        this.$refs.box.style.height = this.height;
+      }
+      this.is_show = !this.is_show;
       this.visible = !this.visible;
       this.$store.dispatch('treeMenusModule/update', { newSelect: `${father}/${value}` });
       this.$store.dispatch('treeMenusModule/updateID', { newSelect: this.nowID });
     },
+    fun_get_list() {
+      // 1：网络请求获取数据后会把元素自动撑开，我们要做的就是让这个元素做展开收起动画
+      // 做法： 1：获取这个元素的高度
+      //        2:吧获取的高度赋值给这个元素，这样这个元素就是高度固定的了。nextTick用于dom刷新后执行
+      this.$nextTick(function () {
+        const height = 100;
+        console.log(height);
+        this.$refs.box.style.height = `${height}px`;
+        this.height = `${height}px`;
+      });
+    },
   },
 });
 </script>
-<style>
+
+<style lang="scss">
+.transition_dom {
+    transition: all 0.3s linear 0s;
+}
+.child_right_slider {
+    display: inline-block;
+    width: 300px;
+    color: white;
+}
 </style>
